78d5b30c7abd6d90bdbf9805e22dccfe3fbd6e03
dorothy <55689863+shonyeajin@users.noreply.github.com>
2026-07-09 18:59:17 +0900

    Delete utils.py

--- utils.py
+++
@@ -1,9 +0,0 @@
-import torch
-from config import PipelineConfig
-
-def generate_a3_causal_mask(group_ids, config: PipelineConfig):
-    B, N = group_ids.shape
-    tgt_group = group_ids.unsqueeze(2).expand(B, N, N)
-    src_group = group_ids.unsqueeze(1).expand(B, N, N)
-    mask = (tgt_group >= src_group) | (src_group == 0)
-    return mask.unsqueeze(1).long()
